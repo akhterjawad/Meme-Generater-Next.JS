3e574189dadc3c6d5cd1927087d97bfed4ee1947
Muhammad Jawad <akhterjawad566@gmail.com>
2024-10-06 14:54:09 +0500

    project initalize

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Home() {
+  return (
+    <React.Fragment>
+      <h1>hello world</h1>
+    </React.Fragment>
+  );
+}
