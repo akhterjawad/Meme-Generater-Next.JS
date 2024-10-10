b22e2e2d2d3bfbbb05dee9d6a699085c6ce9217e
Muhammad Jawad <akhterjawad566@gmail.com>
2024-10-10 15:55:56 +0500

    working

--- app/page.tsx
+++ app/page.tsx
@@ -1,62 +1,51 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 const page = async () => {
-  let memeData = await fetch('https://api.imgflip.com/get_memes')
-  let responce = await memeData.json()
-  console.log(responce.data.memes);
-  interface data {
-    url: string
-    id: string
-    box_count: number
+  let memeData = await fetch('https://api.imgflip.com/get_memes');
+  let response = await memeData.json();
+  console.log(response.data.memes);
+
+  interface Data {
+    url: string;
+    id: string;
+    name: string;
+    box_count: number;
   }
 
   return (
     <React.Fragment>
-      <div>
-        <div className="flex flex-wrap gap-5 items-center justify-center">{responce.data.memes.map((item: data, index: number) => (
-          <Image key={index} src={item.url} width={200} height={200} alt='img' />
-        ))}</div>
+      <div className="container bg-blue-200 mx-auto p-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {response.data.memes.map((item: Data, index: number) => (
+            <div key={item.id} className="flex h-auto flex-col items-center bg-white shadow-md rounded-lg p-4">
+              <Image
+                src={item.url}
+                alt={item.name}
+                width={200} // Adjust width based on box_count for variety
+                height={200} // Adjust height based on box_count for variety
+                className="object-cover rounded-md"
+              />
+              <Button variant="outlined" className="mt-auto">
+                <Link href={{
+                  pathname: "creatememe",
+                  query: {
+                    url: item.url,
+                    id: item.id
+                  }
+                }}>
+                  Generate Meme
+                </Link>
+              </Button>
+            </div>
+          ))}
+        </div>
       </div>
     </React.Fragment>
   );
 }
-export default page
 
-// https://api.imgflip.com/get_memes
-
-
-
-// import Link from 'next/link';
-// import React from 'react'
-
-// interface Users {
-//     name: string;
-//     username: string;
-//     id: number
-// }
-
-// const Product = async () => {
-//     const data = await fetch('https://jsonplaceholder.typicode.com/users')
-//     const response = await data.json()
-//     console.log(response)
-//   return (
-//     <>
-//     <div>Product</div>
-//     <div className='flex justify-center gap-5 flex-wrap'>
-//     {response.map((item: Users)=>{
-//         return <div className='p-5 border border-red-500 rounded'>
-//             <h1 key={item.id}>{item.name} {item.username}</h1>
-//             <button className='btn btn-primary btn-xs'><Link href={`product/${item.id}`}>single user</Link></button>
-//         </div>
-//     })}
-//     </div>
-//     </>
-//   )
-// }
-
-// export default Product
-
-
-
-
+export default page;
